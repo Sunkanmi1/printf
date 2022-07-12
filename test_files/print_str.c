2cd7788049d89c89a20685bbd7bae4a24cc5a7da
SPWELA <sindisiwemp@gmail.com>
2022-07-12 14:43:30 -0700

    test

--- test_files/print_str.c
+++ test_files/print_str.c
@@ -1,4 +1,48 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_char - writes the character c to stdout
+ * @arg: argument
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_char(va_list arg)
+{
+	return (_putchar(va_arg(arg, int)));
+}
+
+/**
+ * print_int - prints an integer.
+ * @arg: argument
+ * Return: 0
+ */
+
+int print_int(va_list arg)
+{
+
+unsigned int divisor = 1, i, resp, charPrinted = 0;
+int n = va_arg(arg, int);
+
+if (n < 0)
+{
+	_putchar('-');
+	charPrinted++;
+	n *= -1;
+}
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+}
+return (charPrinted);
+}
+
 
 /**
  * print_STR - prints a string with a `S` (upper case) specificer
