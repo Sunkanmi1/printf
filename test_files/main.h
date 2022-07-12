b1ec449e10a97fe279e26a5f88a8893991128c57
SPWELA <sindisiwemp@gmail.com>
2022-07-12 14:17:02 -0700

    test string code

---
+++ test_files/main.h
@@ -0,0 +1,27 @@
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+        char *sym;
+        int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/*Main functions*/
+int print_STR(va_list arg);
+int print_str(va_list arg);
+int print_unsigned(va_list arg)
+
+#endif
