910b42255243c3cfd10bcbcc3427539674d64cde
SPWELA <sindisiwemp@gmail.com>
2022-07-13 12:52:02 -0700

    delete _puts file

--- main.h
+++ main.h
@@ -36,10 +36,6 @@ int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
 
-/*_puts functions*/
-int _putchar(char c);
-int _puts(char *str);
-
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
